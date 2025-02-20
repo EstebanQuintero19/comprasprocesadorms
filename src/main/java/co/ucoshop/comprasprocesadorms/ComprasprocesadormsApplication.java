88401fe6cab0c69a53ef5521bf22b446702a835d
ImEsteban <111015349+EstebanQuintero19@users.noreply.github.com>
2025-02-19 21:46:26 -0500

    estructura inicial

---
+++ src/main/java/co/ucoshop/comprasprocesadorms/ComprasprocesadormsApplication.java
@@ -0,0 +1,13 @@
+package co.ucoshop.comprasprocesadorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprasprocesadormsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComprasprocesadormsApplication.class, args);
+	}
+
+}
