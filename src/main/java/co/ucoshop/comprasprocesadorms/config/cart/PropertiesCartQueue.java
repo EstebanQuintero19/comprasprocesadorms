c350d4fcbab2b261c0016667c2c81b017db10a40
Sergio <sergioarias1001@gmail.com>
2025-05-22 20:19:24 -0500

    implementacion rabbitMQ cart

---
+++ src/main/java/co/ucoshop/comprasprocesadorms/config/cart/PropertiesCartQueue.java
@@ -0,0 +1,4 @@
+package co.ucoshop.comprasprocesadorms.config.cart;
+
+public class PropertiesCartQueue {
+}
--- src/main/java/co/ucoshop/comprasprocesadorms/config/comprasQueueConfig.java
+++ src/main/java/co/ucoshop/comprasprocesadorms/config/comprasQueueConfig.java
@@ -4,12 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ConfigurationProperties(prefix = "compras.queues")
+@ConfigurationProperties(prefix = "compras.procesar")
+@PropertySource("classpath:compras.properties")
 @Getter
 @Setter
-public class SaleConfig {
+public class cartQueueConfig {
     private String exchangeName;
     private String routingKey;
     private String queueName;
